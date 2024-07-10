24db3d3918d602aa2ac0d5411ea4aa311b9fc5eb
Kushagra J <volodarolathana@tutanota.com>
2024-07-10 22:29:04 +0530

    refactor(playMorse): improving readability

--- backend/sound.js
+++ backend/sound.js
@@ -2,19 +2,22 @@ const dotSound = new Howl({ src: ['../assets/dot.mp3'] });
 const dashSound = new Howl({ src: ['../assets/dash.mp3'] });
 
 function playMorse(morse) {
-	const characterList = morse.split(' ');
-	for (let char = 0; char < characterList.length; char++) {
-		let dotsAndDashes = characterList[char];
-		for (let i = 0; i < dotsAndDashes.length; i++) {
-			switch (dotsAndDashes[i]) {
+	const characterList = morse.trim().split(' ');
+	characterList.forEach(character => {
+		character.forEach(dotOrDash => {
+			switch (dotOrDash) {
 				case '.':
 					dotSound.play();
 					break;
 				case '-':
 					dashSound.play();
+					break;
 				default:
 					break;
 			}
-		}
-	}
+			setTimeout(() => { }, 500);
+		});
+	});
 }
+
+module.exports = { playMorse };
