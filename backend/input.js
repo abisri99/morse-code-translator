e689c19448494da51e8b51fd1362ad74fd98b387
Kushagra J <volodarolathana@tutanota.com>
2024-07-11 01:05:48 +0530

    feat(sound.js, input.js, index.html, assets): adds audio files and howler script initial implementation

--- backend/input.js
+++ backend/input.js
@@ -1,14 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('morse-button').addEventListener('click', () => {
-      const inputText = document.getElementById('input-text').value;
-      const morseOutput = translateToMorse(inputText);
-      document.getElementById('morse-output').innerText = morseOutput;
-    });
-  
-    document.getElementById('text-button').addEventListener('click', () => {
-      const inputMorse = document.getElementById('input-morse').value;
-      const textOutput = translateToText(inputMorse);
-      document.getElementById('text-output').innerText = textOutput;
-    });
+  document.getElementById('morse-button').addEventListener('click', () => {
+    const inputText = document.getElementById('input-text').value;
+    const morseOutput = translateToMorse(inputText);
+    document.getElementById('morse-output').innerText = morseOutput;
   });
-  
+
+  document.getElementById('text-button').addEventListener('click', () => {
+    const inputMorse = document.getElementById('input-morse').value;
+    const textOutput = translateToText(inputMorse);
+    document.getElementById('text-output').innerText = textOutput;
+  });
+});
+
+document.getElementById('sound').addEventListener('click', () => {
+  playMorse(document.getElementById('input-text').value);
+});
--- backend/sound.js
+++ backend/sound.js
@@ -1,22 +1,27 @@
-const dotSound = new Howl({ src: ['../assets/dot.mp3'] });
-const dashSound = new Howl({ src: ['../assets/dash.mp3'] });
+const dotSound = new Howl({ src: ['../assets/dot.wav'] });
+const dashSound = new Howl({ src: ['../assets/dash.wav'] });
 
-function playMorse(morse) {
-	const characterList = morse.trim().split(' ');
-	characterList.forEach(character => {
-		character.forEach(dotOrDash => {
-			switch (dotOrDash) {
-				case '.':
-					dotSound.play();
-					break;
-				case '-':
-					dashSound.play();
-					break;
-				default:
-					break;
-			}
-			setTimeout(() => { }, 500);
+function playMorse(morseCode) {
+	const characters = morseCode.trim().split(' ');
+	let delay = 0;
+
+	characters.forEach(character => {
+		character.split('').forEach(symbol => {
+			setTimeout(() => {
+				switch (symbol) {
+					case '.':
+						dotSound.play();
+						break;
+					case '-':
+						dashSound.play();
+						break;
+					default:
+						break;
+				}
+			}, delay);
+			delay += 500;
 		});
+		delay += 1000;
 	});
 }
 
