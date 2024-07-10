a2f96683fac0518fdf8dccea843f503b08ee539a
Kushagra J <volodarolathana@tutanota.com>
2024-07-10 23:01:07 +0530

    refactor(input.js): prepares for html

---
+++ backend/input.js
@@ -0,0 +1,7 @@
+let morseBlock = document.getElementById('morse');
+let textBlock = document.getElementById('text');
+let translate = document.getElementById('translate')
+let play = document.getElementById('play');
+
+morseText = morseBlock.innerText();
+text = text.innerText();
