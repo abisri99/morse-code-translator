8399294fc4a3895a4d80da4152e07190c9274aec
Kushagra J <volodarolathana@tutanota.com>
2024-07-11 11:16:10 +0530

    feat(keyboard): hit enter to translate

--- backend/input.js
+++ backend/input.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('morse-button').addEventListener('click', () => {
+  let pattern = /^[ /.\-]*$/;
+  const translate = () => {
     const input = document.getElementById('input-text').value;
-    let pattern = /^[ /.\-]*$/;
     if (pattern.test(input)) {
       const morseOutput = translateToText(input);
       document.getElementById('morse-output').innerText = morseOutput;
@@ -10,8 +10,19 @@ document.addEventListener('DOMContentLoaded', () => {
       const textOutput = translateToMorse(input);
       document.getElementById('morse-output').innerText = textOutput;
     }
-  });
+  }
+  document.getElementById('morse-button').addEventListener('click', translate);
+  document.getElementById('input-text').addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      translate();
+    }
+  })
   document.getElementById('sound').addEventListener('click', () => {
+    const input = document.getElementById('input-text').value;
+    if (pattern.test(input)) {
+      document.getElementById('morse-output').innerText = "Error: Cannot play text output";
+      return;
+    }
     playMorse(document.getElementById('morse-output').innerText);
   });
 });
